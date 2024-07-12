ead7572c5deb6e012bb6aba647a1cba5b4f2a5f7
jayaniprofile <jayanichamodapathirana@gmail.com>
2024-07-12 14:42:09 +0530

    app.js routes

--- mjp-food-products/src/App.js
+++ mjp-food-products/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import OrderOnline from "./components/OrderOnline";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="">
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/order-online" component={OrderOnline} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
