6e3007b58cfebd2adc5da147687edf2caa425886
jayaniprofile <jayanichamodapathirana@gmail.com>
2024-07-12 14:43:16 +0530

    footer

---
+++ mjp-food-products/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 MJP Food Products. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
